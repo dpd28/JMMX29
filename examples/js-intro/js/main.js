c69001e269fadeaed560238fea80ec7135d08929
dpd28 <deb@fastmail.com>
2020-02-24 17:51:27 -0500

    for loop and if else

--- examples/js-intro/js/main.js
+++ examples/js-intro/js/main.js
@@ -54,3 +54,29 @@ const theFruits = [
                 tasty: false,
         },
 ];
+// index can be i or x
+
+// for (let index = 0; index < array.length; index++) {
+// const element = array[index];
+
+// }
+
+// ++ equals by one - increase by 1
+
+for (let index = 0; index < theFruits.length; index++) {
+        // const element = array[index];
+        // check that we are going through the right number of things.
+        // console.log(index);
+        // get the kind of each fruit
+        // console.log(theFruits[index].kind);
+
+        if (theFruits[index].tasty == true) {
+                // fruits is tasty so give me the name
+                console.log(theFruits[index].kind);
+        } else {
+                // fruits are not tasty, ignore
+                //  console.log('Your fruit is on mars');
+                // this is a string literal
+                console.log(`${theFruits[index].kind} belongs on Mars`);
+        }
+}
